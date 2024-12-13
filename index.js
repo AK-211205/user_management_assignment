36f1bdd50bb506f1c2d1a7178bdd5d8fa19ac198
AK-211205 <134520826+AK-211205@users.noreply.github.com>
2024-12-13 19:59:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ app.use(cookieParser());
 
 
 app.use(cors({
-  origin: 'http://localhost:3000', // Frontend URL
+  origin: 'https://user-management-frontend-ashen.vercel.app/', // Frontend URL
   credentials: true, // Allow cookies
 }));
 
